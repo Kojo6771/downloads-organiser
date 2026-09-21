0456d7db4165497c6dc11a9b2ab0e81d2217c07f
Kojo6771 <230165858@aston.ac.uk>
2026-09-21 20:28:21 +0100

    Implemented file extension category mapping

--- organiser.py
+++ organiser.py
@@ -2,7 +2,19 @@ import pathlib as p
 
 def get_category(file_path):
     # Return the category based on the extension.
-    pass
+    ext = p.Path(file_path).suffix.lower()
+    if ext in [".jpg", ".jpeg", ".png", ".gif"]:
+        return "images"
+    elif ext in [".mp4", ".avi", ".mov", ".mkv"]:
+        return "videos"
+    elif ext in [".mp3", ".wav", ".flac"]:
+        return "audio"
+    elif ext in [".pdf", ".doc", ".docx", ".txt"]:
+        return "documents"
+    elif ext in [".zip", ".rar", ".tar", ".gz"]:
+        return "archives"
+    else:
+        return "others"
 
 def organise(folder, apply=False):
     # Inspect files, choose destinations, then preview or move.
